c2a2c5046a810cfe3057057b69f0e35c3850a10c
Duke Lee <dukelee92@yahoo.com>
2024-03-25 14:37:39 -0700

    Update with year/month processing

--- can-woo-orders-batch/process_orders.py
+++ can-woo-orders-batch/process_orders.py
@@ -1,12 +1,13 @@
 import base64
 import functions_framework
-import functions_framework
 import os
 import json
 import requests
+import calendar
 from google.cloud import storage
 from google.cloud import firestore
 from woocommerce import API
+from datetime import datetime, timedelta
 
 # Initialize
 
@@ -31,11 +32,12 @@ wcapi = API(
 
 # Function to process orders
 def process_orders(event, context):
+
     # Decode the Pub/Sub message
     if 'data' in event:
         message_data = base64.b64decode(event['data']).decode('utf-8')
         data = json.loads(message_data)
-        site_name = data.get('site_name') if data else 'CAN'
+        site_name = data.get('site_name') if data else 'CAN-ELSE'
     else:
         print("No data found in event")
         return
@@ -43,7 +45,10 @@ def process_orders(event, context):
     print(f"Processing orders for site: {site_name}")
 
     state_doc_ref = db.collection(f'{site_name}-processing_state').document('woocommerce_orders')
-    state_doc = state_doc_ref.get()
+    month_doc_ref = db.collection(f'{site_name}-processing_state').document('month')
+    year_doc_ref = db.collection(f'{site_name}-processing_state').document('year')
+    #state_doc = state_doc_ref.get()
+    state_doc, month_doc, year_doc = db.get_all([state_doc_ref, month_doc_ref, year_doc_ref])
 
     # Check if the document exists
     if state_doc.exists:
@@ -52,23 +57,54 @@ def process_orders(event, context):
         print(f"No existing document for {site_name}, starting from the beginning.")
         last_processed_page = 0
 
+    if month_doc.exists:
+        last_processed_month = month_doc.to_dict().get('last_processed_month', 1)
+    else:
+        print(f"No existing document for {site_name}, starting from the beginning.")
+        last_processed_month = 1
+
+    if year_doc.exists:
+        last_processed_year = year_doc.to_dict().get('last_processed_year', 2015)
+    else:
+        print(f"No existing document for {site_name}, starting from the beginning.")
+        last_processed_year = 2015
+
     # Fetch the next batch of orders in ascending order
     current_page = last_processed_page + 1
 
+    month = last_processed_month
+    year = last_processed_year
+
+     # Determine the current date
+    num_days = calendar.monthrange(year,month)[1]
+    end_date = datetime(year,month,num_days)
+    end_date_str = end_date.strftime('%Y-%m-%dT23:59:59Z')
+
+    # Start from Jan 2024
+    start_date = datetime(year,month,1)
+    start_date_str = start_date.strftime('%Y-%m-%dT00:00:00Z')
+
     # Fetch orders from the API
-    orders = wcapi.get("orders", params={"page": current_page, "order": "asc"}).json()
+    orders = wcapi.get("orders", params={
+        "after": start_date_str,
+        "before": end_date_str,
+        "page": current_page, 
+        "order": "asc"
+    }).json()
 
     print(f"Fetched {len(orders)} orders from WooCommerce for site: {site_name}")
 
-
-    for order in orders:
-        order_id = order['id']  # Assuming each order has a unique 'id'
-        blob = bucket.blob(f'{site_name}/Orders/Unprocessed/{order_id}.json')
-        try:
-            blob.upload_from_string(json.dumps(order))
-        except Exception as e:
-            print(f"Error uploading order {order_id}: {e}")
-
+    if len(orders) > 0:
+        for order in orders:
+            order_id = order['id']  # Assuming each order has a unique 'id'
+            blob = bucket.blob(f'{site_name}/Orders/Unprocessed/{year}/{month}/{order_id}.json')
+            try:
+                blob.upload_from_string(json.dumps(order))
+            except Exception as e:
+                print(f"Error uploading order {order_id}: {e}")
+    else:
+        print("No orders found")
+        
     # Update the state in Firestore
     state_doc_ref.set({'last_processed_page': current_page})
 
@@ -76,3 +112,11 @@ def process_orders(event, context):
     if len(orders) < 10:
         state_doc_ref.set({'last_processed_page': 0})
         print(f"ALL ORDERS COMPLETED for {site_name}")
+        # Move to the next month
+        if month == 12:
+            next_year = year + 1
+            year_doc_ref.set({'last_processed_year': next_year})
+            month_doc_ref.set({'last_processed_month': 1})
+        else:
+            month += 1
+            month_doc_ref.set({'last_processed_month': month})
